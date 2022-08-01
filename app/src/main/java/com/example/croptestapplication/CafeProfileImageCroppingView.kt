919ccc2061fd2f4da2ee9ed9154dfbbb0b65f2bb
SungChulByun <sungchul.byun@navercorp.com>
2022-08-02 01:58:40 +0900

    a

--- app/src/main/java/com/example/croptestapplication/CafeProfileImageCroppingView.kt
+++ app/src/main/java/com/example/croptestapplication/CafeProfileImageCroppingView.kt
@@ -11,6 +11,16 @@ import androidx.core.graphics.scale
 import kotlin.math.max
 import kotlin.math.min
 
+/**
+    1. 사진 불러온다.
+    2. 화면 크기에 맞게 사진 크기 조정(계산)
+    3. (축소할 수 있는 최소크기 결정 필요) (계산) - 큰 이미지 고려 (화면 사이즈의 3배수 넘지 않도록)
+    4. 화면 터치 이벤트 처리
+    5. 크롭 할 위치 계산
+    6. 크롭 -> 비트맵
+
+ load, updateUI, clear, ...
+ */
 class CafeProfileImageCroppingView constructor(context: Context, attrs: AttributeSet) :
     View(context, attrs) {
 
@@ -23,7 +33,7 @@ class CafeProfileImageCroppingView constructor(context: Context, attrs: Attribut
     inner class ScaleListener : ScaleGestureDetector.SimpleOnScaleGestureListener() {
         override fun onScale(scaleGestureDetector: ScaleGestureDetector): Boolean {
             scaleFactor *= scaleGestureDetector.scaleFactor
-            scaleFactor = max(1f, min(scaleFactor, 5.0f))
+            scaleFactor = max(0.5f, min(scaleFactor, 5.0f))
             this@CafeProfileImageCroppingView.scaleX = scaleFactor
             this@CafeProfileImageCroppingView.scaleY = scaleFactor
 
@@ -31,8 +41,19 @@ class CafeProfileImageCroppingView constructor(context: Context, attrs: Attribut
         }
     }
 
-    var scaledBitmap: Bitmap? = null
-    var croppedBitmap: Bitmap? = null
+    override fun onFinishInflate() {
+        super.onFinishInflate()
+    }
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+        this.setOnTouchListener (onTouchListener)
+    }
+
+
+//    var scaledBitmap: Bitmap? = null
+    var targetBitmap: Bitmap? = null
+
     var sourceBitmap: Bitmap? = null
         set(value) {
             field = value
@@ -61,6 +82,10 @@ class CafeProfileImageCroppingView constructor(context: Context, attrs: Attribut
 
         scaledBitmap = Bitmap.createBitmap(sourceBitmap!!)
             .scale((sourceBitmap!!.width * scale).toInt(), (sourceBitmap!!.height * scale).toInt())
+
+        // crop 할 때 원본 비트맾에서 해야 화질 유지
+        // .scale 리소스 많이 먹음. 5만, 10만 이러면 죽음
+        // 37줄 처럼 처리하는게 나음
     }
 
     private var redPaint = Paint().apply {
@@ -87,14 +112,16 @@ class CafeProfileImageCroppingView constructor(context: Context, attrs: Attribut
         super.onDraw(canvas)
         canvas.drawColor(Color.RED)
 
-        scaledBitmap?.let {
-            canvas.drawBitmap(it,
-                0f,
-                (height - scaledBitmap!!.height) / 2f,
-                redPaint)
-        }
         canvas.save()
         canvas.translate(width / 2f, height / 2f)
+        canvas.scale()
+        val matrix = Matrix()
+        canvas.concat(matrix)
+
+
+        drawSource(canvas)
+
+        canvas.restore()
 
         if (createBox) {
             Log.d("test_by_sungchul",
@@ -126,6 +153,13 @@ class CafeProfileImageCroppingView constructor(context: Context, attrs: Attribut
         }
     }
 
+    private fun drawSource(canvas: Canvas) {
+        scaledBitmap?.let {
+//            canvas.drawBitmap(it, 0f, (height - scaledBitmap!!.height) / 2f, redPaint)
+            canvas.drawBitmap(it, 0f, 0f, redPaint)
+        }
+    }
+
     private var startX = 0f
     private var startY = 0f
     private val sx = 0f
@@ -138,7 +172,11 @@ class CafeProfileImageCroppingView constructor(context: Context, attrs: Attribut
             return true
         }
 
+        // 위에 제스처 디텍터
         scaleGestureDetector.onTouchEvent(event)
+
+        // return gestureDetector.onTouchEvent(event) || scaleGestureDetector.onTouchEvent(event)
+        // 보통 return scaleGestureDetector.onTouchEvent(event) 이렇게 한다.
 
         when (event.actionMasked) {
             MotionEvent.ACTION_DOWN -> {
@@ -155,9 +193,24 @@ class CafeProfileImageCroppingView constructor(context: Context, attrs: Attribut
                     y += event.y - startY
                 }
             }
+            MotionEvent.ACTION_CANCEL -> {
+                // 처리하다가 중간에 없어진 경우 (화면 밖으로 나가는 경우)
+            }
         }
-        return true
+        // event 순서 2번
+        return true // consume 메세지, true면 내가 처리하겠다는 의미. false면 다른 view한테
     }
+
+    // event 순서 1번
+    override fun dispatchTouchEvent(event: MotionEvent?): Boolean {
+        return super.dispatchTouchEvent(event)
+    }
+
+    // event 순서 3번
+    private val onTouchListener(motionEvent: MotionEvent?): Unit {
+
+    }
+
 
     fun createBox() {
         createBox = true
